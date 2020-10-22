a9b6db606463571a2837001c834cda6929caa3b4
Valery Filipev <valeranuzdin1998@mail.ru>
2020-10-22 21:54:27 +0300

    add back-end for the authentication without front-end set up yet

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const usersRoutes = require("./routes/users");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
+
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.dgw8f.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(error => {
+    console.log(error);
+  });
