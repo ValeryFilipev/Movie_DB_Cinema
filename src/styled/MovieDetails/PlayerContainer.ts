eb5b8f6ce07410618e2ad87053360389cb4ed562
Valery Filipev <valeranuzdin1998@mail.ru>
2020-11-29 01:42:47 +0300

    finish the trailer feature

---
+++ src/styled/MovieDetails/PlayerContainer.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const PlayerContainer = styled.div`
+  overflow: auto;
+  white-space: nowrap;
+`;
+
+export default PlayerContainer;
---
+++ src/styled/MovieDetails/PlayerItem.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { getSpace } from '../../helpers/theme';
+
+const PlayerItem = styled.div`
+  display: inline-block;
+  padding: ${getSpace(1)};
+`;
+
+export default PlayerItem;
